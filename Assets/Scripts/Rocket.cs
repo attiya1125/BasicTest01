a46ade09a07c9306d17aadba2ba4fb9abf75e1f3
attiya1125 <leonrey1125@gmail.com>
2024-05-14 14:52:26 +0900

    Day_02

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -7,7 +7,7 @@ public class Rocket : MonoBehaviour
 {
     private Rigidbody2D _rb2d;
     private float fuel = 100f;
-    
+
     private readonly float SPEED = 5f;
     private readonly float FUELPERSHOOT = 10f;
 
@@ -15,23 +15,20 @@ public class Rocket : MonoBehaviour
     [SerializeField] private TextMeshProUGUI HighScoreTxt;
 
     public Transform rocket;
-    void Awake()
+    private void Awake()
     {
         // TODO : Rigidbody2D 컴포넌트를 가져옴(캐싱) 
-        _rb2d = GetComponent<Rigidbody2D>();
+        _rb2d = gameObject.GetComponent<Rigidbody2D>();
     }
-    
+
     public void Shoot()
     {
         // TODO : fuel이 넉넉하면 윗 방향으로 SPEED만큼의 힘으로 점프, 모자라면 무시
-        if (fuel >= 10)
+        if (fuel >= FUELPERSHOOT)
         {
-            _rb2d.AddForce(Vector2.up * SPEED);
+            _rb2d.AddForce(Vector2.up * SPEED, ForceMode2D.Impulse);
             fuel -= FUELPERSHOOT;
-        }
-        else
-        {
-
+            Debug.Log("남은 연료량" + fuel);
         }
     }
     private void Update()
